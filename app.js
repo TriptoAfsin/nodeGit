12c832398fb8ce6fb5a32210ed01010163b74400
TriptoAfsin <afsintripto@gmail.com>
2020-07-10 23:24:53 +0600

    third commit

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 
 const app = express();
 
-const http = require('http')
+const http = require('http');
+const { parse } = require('path');
+const { json } = require('express');
 
 
 
@@ -38,7 +40,7 @@ app.get('/api/randomNum', (req, res)=> {
   
             let myNum = String(Math.floor(Math.random()*100))
             
-            return res.status(200).send(myNum);
+            return res.status(200).send(parse(myNum));
   
         }catch(error){
             console.log(error);
@@ -48,6 +50,27 @@ app.get('/api/randomNum', (req, res)=> {
   
     })();
 });
+
+app.get('/api/pi:value', (req, res)=> {
+    (async () => {
+  
+        try{
+            let value = req.params.value.replace(":", "");
+  
+            let output = String(parseFloat(value)*3.1416);
+            console.log(output)
+            
+            return res.status(200).send(parse(output));
+  
+        }catch(error){
+            console.log(error);
+            return res.status(500).send(error);
+            
+        }
+  
+    })();
+});
+
 
 //for hosting env
 
